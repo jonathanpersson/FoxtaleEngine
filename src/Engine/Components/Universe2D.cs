5c23d1ad95e1eed27da3127b08fdc7675fdbe58c
Jonathan Persson <hello@jonathanpersson.net>
2023-05-19 18:49:47 +0200

    start converting world to ECS

--- src/Engine/Components/Universe2D.cs
+++ src/Engine/Components/Universe2D.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using MgGame.Engine.Entities;
 using MgGame.Engine.Systems;
@@ -6,10 +7,12 @@ namespace MgGame.Engine.Components;
 
 public class Universe2D : IComponent
 {
+    List<IEntity> _content;
     public IEntity Entity { get; set; }
 
     public Universe2D()
     {
+        _content = new();
         Universe2DSystem.AddComponent(this);
     }
 
@@ -20,6 +23,6 @@ public class Universe2D : IComponent
 
     public void Update(GameTime gameTime)
     {
-        
+
     }
 }
