e2a27cf5b494b4bec23a971478f70681c9b88774
Jonathan Persson <hello@jonathanpersson.net>
2023-06-13 20:33:23 +0200

    VERY basic ECS and UI system is functional!

--- src/Engine/Components/Sprite.cs
+++ src/Engine/Components/Sprite.cs
@@ -2,6 +2,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MgGame.Engine.Entities;
 using MgGame.Engine.Systems;
+using MgGame.Engine.Components.UI;
 
 namespace MgGame.Engine.Components;
 
@@ -10,7 +11,23 @@ public class Sprite : IComponent
     public Texture2D Texture { get; set; }
     public IEntity Entity { get; set; }
 
-    public Sprite() { }
+    public Color RenderColor
+    {
+        get
+        {
+            //todo: inherit style?
+            if (Entity.TryGetComponent(out Style style))
+            {
+                return style.ForegroundColor;
+            }
+            return Color.White;
+        }
+    }
+
+    public Sprite()
+    {
+        SpriteSystem.AddComponent(this);
+    }
 
     public Sprite(Texture2D spriteTexture)
     {
--- src/Engine/Systems/SpriteSystem.cs
+++ src/Engine/Systems/SpriteSystem.cs
@@ -1,5 +1,18 @@
 using MgGame.Engine.Components;
+using MgGame.Engine.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace MgGame.Engine.Systems;
 
-public class SpriteSystem : BaseSystem<Sprite> {}
+public class SpriteSystem : BaseSystem<Sprite>
+{
+    public static void Draw(SpriteBatch sb)
+    {
+        foreach (Sprite sprite in components)
+        {
+            Vector2 location = sprite.Entity.GetComponent<Transform2D>().Position;
+            sb.Draw(sprite.Texture, location, sprite.RenderColor);
+        }
+    }
+}
