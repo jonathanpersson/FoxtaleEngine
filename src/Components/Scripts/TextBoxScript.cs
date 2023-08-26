3fc948eee9f73e97c0919556681e3372d5878ce7
Jonathan <hello@jonathanpersson.net>
2023-08-26 14:30:47 +0200

    BUGFIX: Including WIP TextBox entity to prevent angry compiler heh

--- src/Components/Scripts/TextBoxScript.cs
+++ src/Components/Scripts/TextBoxScript.cs
@@ -1,6 +1,19 @@
+using Foxtale.Entities.UI.Controls;
+using Microsoft.Xna.Framework;
+
 namespace Foxtale.Components.Scripts;
 
-public class TextBoxScript
+public class TextBoxScript : Script
 {
+    public TextBox Text { get; }
     
+    public TextBoxScript(TextBox textBox)
+    {
+        Text = textBox;
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        
+    }
 }
--- src/Entities/UI/Controls/TextBox.cs
+++ src/Entities/UI/Controls/TextBox.cs
@@ -1,6 +1,25 @@
+using Foxtale.Components;
+using Foxtale.Components.Scripts;
+using Foxtale.Components.UI;
+
 namespace Foxtale.Entities.UI.Controls;
 
-public class TextBox
+public class TextBox : UIEntity
 {
-    
+    public Text Content { get; }
+    public Event OnInput { get; }
+    public Event OnClick { get; }
+    public TextBoxScript Script { get; }
+
+    public TextBox(string content = null)
+    {
+        Content = new Text(content ?? string.Empty);
+        OnInput = new Event();
+        OnClick = new Event();
+        Script = new TextBoxScript(this);
+        AddComponent(Content);
+        AddComponent(OnInput);
+        AddComponent(OnClick);
+        AddComponent(Script);
+    }
 }
